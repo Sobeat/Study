2adebbf391bea887ea349573a7d59f147cc1edba
Sobeat <wnsdud2090@gmail.com>
2022-04-03 15:58:31 +0900

    백준 알고리즘 기초1

--- c#/Programe.cs
+++ c#/Programe.cs
@@ -5,16 +5,7 @@ namespace Programe
         static void Main(string[] args)
         {
             Study study = new Study();
-            //math.Baekjoon_2609();
-            //math.Baekjoon_17425();
-            
-            //math.Baekjoon_17427();
-            string[] enroll = {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"};
-            string[] referral = {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"};
-            string[] seller = {"young", "john", "tod", "emily", "mary"};
-            int[] amount = {12, 4, 2, 5, 10};
-            study.WebBackEnd_2021_Lv3(enroll, referral, seller, amount);
-            //[360, 958, 108, 0, 450, 18, 180, 1080]
+            study.Baekjoon_1406();
         }
     }
 }
